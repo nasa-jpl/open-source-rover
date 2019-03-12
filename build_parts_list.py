5316a31ff534d6c47ea916341ce86ca2ef7d4b2b
Mik Cox <cox@jpl.nasa.gov>
2019-03-12 14:15:07 -0700

    Now generating formatted .xlsx file to match original parts list. Finalized build_parts_list.py.

--- build_parts_list.py
+++ build_parts_list.py
@@ -2,6 +2,7 @@ import os
 import re
 import csv
 import glob
+from xlsxwriter.workbook import Workbook
 import math
 from collections import defaultdict
 
@@ -97,8 +98,8 @@ for part_ref, part_data in sub_assembly_part_counts.items():
                               'Used in Sections': ', '.join(list(part_data['sections']))})
     print("\t", part_ref, "-", part_data['name'])
 
-
-with open('master_parts_list.csv', 'w') as f:
+# Write a master .csv file 
+with open('master_parts_list_raw.csv', 'w') as f:
     # Hardcode the list to be in the order that we want
     fieldnames = ['Part Name',
                   'Project Ref Code',
@@ -116,3 +117,52 @@ with open('master_parts_list.csv', 'w') as f:
 
     writer.writeheader()
     writer.writerows(master_parts_list)
+
+# We also wish to create a formatted .xlsx file with the JPL disclaimers and format that matches the old master parts list
+workbook = Workbook('master_parts_list.xlsx', {'strings_to_numbers': True})
+worksheet = workbook.add_worksheet('Sheet1')
+# Write header information
+worksheet.write('A1', 'The cost information contained in this document is of a budgetary and planning nature and is intended for informational purposes only.  It does not constitute a commitment on the part of JPL and/or Caltech.')
+worksheet.write('A2', 'Authors: Eric Junkins, Michael (Mik) Cox, Jet Propulsion Laboratory, California Institute of Technology')
+worksheet.write('A3', '© 2018 California Institute of Technology. Government sponsorship acknowledged')
+
+# Cell formats
+bold_format = workbook.add_format({'bold': True, 'font_size': 14})
+column_header_format = workbook.add_format({'bold': True, 'font_size': 14, 'align': 'center', 'valign': 'vcenter', 'text_wrap': True})
+price_column_format = workbook.add_format({'num_format': '"$"#,##0.00'})
+price_total_format = workbook.add_format({'bold': True, 'font_size': 14, 'num_format': '"$"#,##0.00'})
+
+# Create total cost box
+worksheet.write('J2', 'Total')
+worksheet.write_formula('K2', '=SUM(J5:J200)', price_total_format)
+
+with open('master_parts_list_raw.csv', 'r') as csvfile:
+    table = csv.reader(csvfile)
+    i = 3
+    for row in table:
+        # Strip number signs off of .csv values
+        row[8] = row[8].replace('$', '')
+        row[9] = row[9].replace('$', '')
+        worksheet.write_row(i, 0, row)
+        i += 1
+
+# Format the main header information & total cost box
+worksheet.set_row(0, 30, bold_format)
+worksheet.set_row(1, 30, bold_format)
+worksheet.set_row(2, 30, bold_format)
+# Format the column headers and price columns
+worksheet.set_row(3, 60, column_header_format)
+worksheet.autofilter('A4:K4')
+worksheet.set_column('A:A', 37)
+worksheet.set_column('B:B', 12)
+worksheet.set_column('C:C', 26)
+worksheet.set_column('D:D', 12.5)
+worksheet.set_column('E:E', 40)
+worksheet.set_column('F:F', 12)
+worksheet.set_column('G:G', 12)
+worksheet.set_column('H:H', 12)
+worksheet.set_column('I:I', 12, price_column_format)
+worksheet.set_column('J:J', 12, price_column_format)
+worksheet.set_column('K:K', 25)
+
+workbook.close()
